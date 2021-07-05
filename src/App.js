281caeb842d4df62c6430b83c6f6b87ede8c01ca
devgladstone <mistuhgladstone@gmail.com>
2021-07-05 10:40:51 -0700

    add sidebar layout

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import AdminPage from "./component/AdminPage";
 import UserPage from "./component/UserPage";
+import SidebarLayout from "./component/SidebarLayout";
 import Container from "./component/Container";
 
 function App() {
   return (
-    <div className="">
-      <Container>
-        <Switch>
-          <Route path="/admin">
-            <AdminPage />
-          </Route>
-          <Route path="/">
-            <UserPage />
-          </Route>
-        </Switch>
-      </Container>
-    </div>
+    <SidebarLayout>
+      <Switch>
+        <Route path="/admin">
+          <AdminPage />
+        </Route>
+        <Route path="/">
+          <UserPage />
+        </Route>
+      </Switch>
+    </SidebarLayout>
   );
 }
 
