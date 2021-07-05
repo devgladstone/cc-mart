e281099c7245c5a78b637405fedbad304ec9c417
devgladstone <mistuhgladstone@gmail.com>
2021-07-05 16:04:04 -0700

    clean sidebar

--- src/component/SidebarLayout.js
+++ src/component/SidebarLayout.js
@@ -192,7 +192,7 @@ export default function SidebarLayout({ children }) {
                   </div>
                   <div className="ml-3">
                     <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
-                      Guest user
+                      {pathname === "/admin" ? "Admin User" : "Guest user"}
                     </p>
                   </div>
                 </div>
@@ -214,12 +214,7 @@ export default function SidebarLayout({ children }) {
         <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none">
           <div className="py-6">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-              {/* Replace with your content */}
-              {/* <div className="py-4">
-                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-              </div> */}
               {children}
-              {/* /End replace */}
             </div>
           </div>
         </main>
